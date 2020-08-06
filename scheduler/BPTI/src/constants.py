13ce604fe40c7ad81ad09befd479f9284ff72b30
shreshthtuli <shreshthtuli@gmail.com>
2020-08-06 15:40:55 +0530

    Added energy+latency model

--- scheduler/BPTI/src/constants.py
+++ scheduler/BPTI/src/constants.py
@@ -2,3 +2,5 @@ EPOCHS = 100
 MODEL_SAVE_PATH = 'checkpoints'
 
 POPULATION_SIZE = 100
+Coeff_Energy = 0.8
+Coeff_Latency = 0.2
--- scheduler/BPTI/src/models.py
+++ scheduler/BPTI/src/models.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+from .constants import *
+
+from sys import argv
 
 class energy(nn.Module):
     def __init__(self):
@@ -19,3 +22,25 @@ class energy(nn.Module):
         x = x.flatten()
         x = self.find(x)
         return x
+
+class energy_latency(nn.Module):
+    def __init__(self):
+        super(energy, self).__init__()
+        self.name = "energy_latency"
+        self.find = nn.Sequential(
+            nn.Linear(50 * 51, 128),
+            nn.Softplus(),
+            nn.Linear(128, 128),
+            nn.Softplus(),
+            nn.Linear(128, 64), 
+            nn.Tanhshrink(),
+            nn.Linear(64, 2),
+            nn.Sigmoid())
+
+    def forward(self, x):
+        x = x.flatten()
+        x = self.find(x)
+        if not('train' in argv[0] and 'train' in argv[1]):
+            x = Coeff_Energy*x[0] + Coeff_Latency*x[1]
+        return x
+
