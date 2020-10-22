97c5829ddfffd8dc7d4b553c9dacf2c72c34d1f3
Shreshth Tuli <shreshthtuli@gmail.com>
2020-10-22 08:08:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def initalizeEnvironment(environment, logger):
 	
 	# Initialize scheduler
 	''' Can be LRMMTR, RF, RL, RM, Random, RLRMMTR, TMCR, TMMR, TMMTR, GA, GOBI (arg = 'energy_latency_'+str(HOSTS)) '''
-	scheduler = GOBIScheduler('energy_latency_'+str(HOSTS))
+	scheduler = DRLScheduler('energy_latency_'+str(HOSTS))
 
 	# Initialize Environment
 	hostlist = datacenter.generateHosts()
