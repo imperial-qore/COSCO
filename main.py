1cfc271ec8be106a721c7295724a7174fe3adc8f
Shreshth Tuli <shreshthtuli@gmail.com>
2021-06-05 23:48:33 +0100

    Removed imports not needed in tutorial

--- main.py
+++ main.py
@@ -22,32 +22,10 @@ from framework.workload.DeFogWorkload import *
 from simulator.Simulator import *
 from simulator.environment.AzureFog import *
 from simulator.environment.BitbrainFog import *
-from simulator.workload.BitbrainWorkload_GaussianDistribution import *
 from simulator.workload.BitbrainWorkload2 import *
 
 # Scheduler imports
-from scheduler.IQR_MMT_Random import IQRMMTRScheduler
-from scheduler.MAD_MMT_Random import MADMMTRScheduler
-from scheduler.MAD_MC_Random import MADMCRScheduler
-from scheduler.LR_MMT_Random import LRMMTRScheduler
-from scheduler.Random_Random_FirstFit import RFScheduler
-from scheduler.Random_Random_LeastFull import RLScheduler
-from scheduler.RLR_MMT_Random import RLRMMTRScheduler
-from scheduler.Threshold_MC_Random import TMCRScheduler
 from scheduler.Random_Random_Random import RandomScheduler
-from scheduler.HGP_LBFGS import HGPScheduler
-from scheduler.GA import GAScheduler
-from scheduler.GOBI import GOBIScheduler
-from scheduler.GOBI2 import GOBI2Scheduler
-from scheduler.DRL import DRLScheduler
-from scheduler.DQL import DQLScheduler
-from scheduler.POND import PONDScheduler
-from scheduler.SOGOBI import SOGOBIScheduler
-from scheduler.SOGOBI2 import SOGOBI2Scheduler
-from scheduler.HGOBI import HGOBIScheduler
-from scheduler.HGOBI2 import HGOBI2Scheduler
-from scheduler.HSOGOBI import HSOGOBIScheduler
-from scheduler.HSOGOBI2 import HSOGOBI2Scheduler
 
 # Auxiliary imports
 from stats.Stats import *
@@ -64,13 +42,13 @@ parser.add_option("-m", "--mode", action="store", dest="mode", default="0",
 opts, args = parser.parse_args()
 
 # Global constants
-NUM_SIM_STEPS = 100
-HOSTS = 10 * 5 if opts.env == '' else 10
+NUM_SIM_STEPS = 20
+HOSTS = 10 if opts.env == '' else 2
 CONTAINERS = HOSTS
 TOTAL_POWER = 1000
 ROUTER_BW = 10000
 INTERVAL_TIME = 300 # seconds
-NEW_CONTAINERS = 0 if HOSTS == 10 else 5
+NEW_CONTAINERS = 0
 DB_NAME = ''
 DB_HOST = ''
 DB_PORT = 0
@@ -101,7 +79,7 @@ def initalizeEnvironment(environment, logger):
 	
 	# Initialize scheduler
 	''' Can be LRMMTR, RF, RL, RM, Random, RLRMMTR, TMCR, TMMR, TMMTR, GA, GOBI (arg = 'energy_latency_'+str(HOSTS)) '''
-	scheduler = GOBIScheduler('energy_latency_'+str(HOSTS)) # GOBIScheduler('energy_latency_'+str(HOSTS))
+	scheduler = RandomScheduler()
 
 	# Initialize Environment
 	hostlist = datacenter.generateHosts()
@@ -109,6 +87,9 @@ def initalizeEnvironment(environment, logger):
 		env = Framework(scheduler, CONTAINERS, INTERVAL_TIME, hostlist, db, environment, logger)
 	else:
 		env = Simulator(TOTAL_POWER, ROUTER_BW, scheduler, CONTAINERS, INTERVAL_TIME, hostlist)
+
+	# Initialize stats
+	stats = Stats(env, workload, datacenter, scheduler)
 
 	# Execute first step
 	newcontainerinfos = workload.generateNewContainers(env.interval) # New containers info
@@ -123,8 +104,6 @@ def initalizeEnvironment(environment, logger):
 	print("Schedule:", env.getActiveContainerList())
 	printDecisionAndMigrations(decision, migrations)
 
-	# Initialize stats
-	stats = Stats(env, workload, datacenter, scheduler)
 	stats.saveStats(deployed, migrations, [], deployed, decision, schedulingTime)
 	return datacenter, workload, scheduler, env, stats
 
