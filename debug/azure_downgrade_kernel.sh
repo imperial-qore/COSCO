3374b03a10abd8252e092f9d3b352bf51a889569
Shreshth Tuli <shreshthtuli@gmail.com>
2022-03-23 20:17:03 +0000

    Update azure_downgrade_kernel.sh

--- debug/azure_downgrade_kernel.sh
+++ debug/azure_downgrade_kernel.sh
@@ -5,8 +5,8 @@ linux-headers-4.15.0-1009-azure \
 linux-modules-4.15.0-1009-azure \
 linux-modules-extra-4.15.0-1009-azure
 
-sudo apt remove -y -f linux-headers-5.4.0-1036-azure \
-linux-image-5.4.0-1036-azure \
-linux-image-unsigned-5.4.0-1036-azure
+sudo apt remove -y -f linux-headers-$(uname -r) \
+linux-image-$(uname -r) \
+linux-image-unsigned-$(uname -r)
 
 sudo reboot
