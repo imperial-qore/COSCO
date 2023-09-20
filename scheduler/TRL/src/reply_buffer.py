1bb4b8ee9ad545016921d23f43f29dd2b804ec17
ImMohammadHosseini <sayedmh50@gmail.com>
2023-09-20 17:04:42 +0330

    add and change methods

--- scheduler/TRL/src/reply_buffer.py
+++ scheduler/TRL/src/reply_buffer.py
@@ -3,55 +3,76 @@
 
 """
 import torch
-from typing import Optional
+from typing import Optional, List
+import numpy as np
 
 class PPOReplayBuffer ():
     def __init__ (self, link_number):
-        self.link_number = link_number
-        self.reset_normal_memory()
-        self.reset_extra_memory()
+        #self.link_number = link_number
+        self.reset_final_memory()
+        self.reset_mid_memory()
         
     
-    def reset_normal_memory (self) :
-        self.normal_observation = [] 
-        self.normal_internalObservation = []
-        self.normal_action = [] 
-        self.normal_prob = [] 
-        self.normal_val = []
-        self.normal_reward = []
-        self.normal_steps = []
-        self.normal_done = []
+    def reset_final_memory (self) :
+        self.final_observation = [] 
+        self.final_internalObservation = []
+        self.final_decision = [] 
+        self.final_action = [] 
+        self.final_prob = [] 
+        self.final_val = []
+        self.final_reward = []
+        self.final_steps = []
         
-    def save_normal_step (
+    def reset_mid_memory (self) :
+        self.mid_observation = [] 
+        self.mid_internalObservation = []
+        self.mid_decision = [] 
+        self.mid_action = [] 
+        self.mid_prob = [] 
+        self.mid_val = []
+        self.mid_steps = []
+    
+    def save_mid_step (
         self, 
-        observation: torch.tensor,
-        actions: torch.tensor,
-        probs: torch.tensor,
-        values: torch.tensor, 
-        rewards: torch.tensor,
-        done: bool,
-        steps: Optional[torch.tensor] = None,
-        internalObservations: Optional[torch.tensor] = None,
-
+        observation: List[torch.Tensor],
+        decision: List,
+        actions: List,
+        probs: List,
+        values: List, 
+        steps: Optional[List[torch.Tensor]] = None,
+        internalObservations: Optional[List[torch.Tensor]] = None,
     ):
-        self.normal_observation.append(torch.cat([observation.unsqueeze(1)]*self.link_number, 1).cpu())
-        self.normal_action.append(torch.cat(actions,1).cpu())
-        self.normal_prob.append(torch.cat(probs,1).cpu())
-        self.normal_val.append(torch.cat(values,1).cpu())
-        self.normal_reward.append(torch.cat(rewards,1).cpu())
+        self.mid_observation += observation
+        self.mid_decision += decision
+        self.mid_action += actions
+        self.mid_prob += probs
+        self.mid_val += values
         
-        if done == True:
-            self.normal_done.append(torch.tensor([False]*(self.link_number-1)+[True]).unsqueeze(0))
-        else:
-            self.normal_done.append(torch.tensor([done]*self.link_number).unsqueeze(0))
-            
         if not isinstance(steps, type(None)):
-            self.normal_steps.append(steps.unsqueeze(0).cpu())
-
+            self.mid_steps += steps
+            
         if not isinstance(internalObservations, type(None)):
-            self.normal_internalObservation.append(torch.cat(internalObservations,0).unsqueeze(0).cpu())
+            self.mid_internalObservation += internalObservations
 
-    def get_normal_memory (self):
+    def save_final_memory (
+        self,
+        rewards:dict
+    ):
+        for i in rewards:
+            condition = (np.array(self.mid_decision)[:,0] == i[0]) &  (np.array(self.mid_decision)[:,1] == i[1])
+            index = np.where(condition)[0][0]
+            self.final_observation.append(self.mid_observation.pop(index))
+            self.final_decision.append(self.mid_decision.pop(index)) 
+            self.final_action.append(self.mid_action.pop(index))
+            self.final_prob.append(self.mid_prob.pop(index))
+            self.final_val.append(self.mid_val.pop(index))
+            self.final_reward.append(rewards[i])
+            
+            try:
+                self.final_internalObservation.append(self.mid_internalObservation.pop(index))
+                self.final_steps.append(self.mid_steps.pop(index))
+        
+    def get_memory (self):
         try: return torch.cat(self.normal_observation, 0), \
                 torch.cat(self.normal_action, 0), \
                 torch.cat(self.normal_prob, 0), \
